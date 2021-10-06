4ad9b0f7796d262019dfe3dd71e0a84de0aaaa58
HorrorTroll <sonicvipduc@gmail.com>
2021-10-06 17:50:35 +0700

    Enable OpenRGB for Handwired K552 Kurama

--- keyboards/handwired/horrortroll/k552/keymaps/openrgb/config.h
+++ keyboards/handwired/horrortroll/k552/keymaps/openrgb/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 Massdrop Inc.
+Copyright 2021 HorrorTroll
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,17 +15,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef _UDI_DEVICE_EPSIZE_H_
-#define _UDI_DEVICE_EPSIZE_H_
+#pragma once
 
-#define KEYBOARD_EPSIZE 8
-#define MOUSE_EPSIZE 8
-#define EXTRAKEY_EPSIZE 8
-#define RAW_EPSIZE 64
-#define CONSOLE_EPSIZE 32
-#define NKRO_EPSIZE 32
-#define MIDI_STREAM_EPSIZE 64
-#define CDC_NOTIFICATION_EPSIZE 8
-#define CDC_EPSIZE 16
-
-#endif  //_UDI_DEVICE_EPSIZE_H_
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
