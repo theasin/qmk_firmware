40fdac71adc6e0383e964625f9a1833ad212f5ba
HorrorTroll <sonicvipduc@gmail.com>
2022-05-05 16:25:57 +0700

    240: Fixed issue for not booting keyboard and enable memcore

--- platforms/chibios/boards/SN_SN32F240/configs/chconf.h
+++ platforms/chibios/boards/SN_SN32F240/configs/chconf.h
@@ -52,7 +52,7 @@
  *          setting also defines the system tick time unit.
  */
 #if !defined(CH_CFG_ST_FREQUENCY)
-#define CH_CFG_ST_FREQUENCY                 100000
+#define CH_CFG_ST_FREQUENCY                 10000
 #endif
 
 /**
@@ -338,7 +338,7 @@
  * @note    The default is @p TRUE.
  */
 #if !defined(CH_CFG_USE_MEMCORE)
-#define CH_CFG_USE_MEMCORE                  FALSE
+#define CH_CFG_USE_MEMCORE                  TRUE
 #endif
 
 /**
